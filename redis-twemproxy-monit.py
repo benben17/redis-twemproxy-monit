d7ec0e1f2b3ddfd82ddbfd4177ee4a844f6be45c
benben17 <892100089@qq.com>
2015-10-10 14:28:37 +0800

    log  输出更新

--- redis-twemproxy-monit.py
+++ redis-twemproxy-monit.py
@@ -5,8 +5,7 @@ import re
 import time,datetime
 import paramiko
 
-__author__ = 'lixiaozhu'
-#配置相关命令
+
 #nutcracker文件位置
 nutcracker_file="/etc/nutcracker.yml"
 #启动sentinel
@@ -25,16 +24,20 @@ check_time=10
 
 #配置相关服务器信息
 host={}
-host["ip"]="10.xx.xx.xx"
-host["name"]="********"
-host["password"]="********"
+host["ip"]="10.237.81.xxx"
+host["name"]="****
+host["password"]="*******"
 host["port"]=22
 #检查各个进程的存活情况
 redis_check="netstat -antp|awk  '/7000/ &&  /LISTEN/'"
-redis_sentinel_cehck="netstat -antp|awk  '/7000/ &&  /LISTEN/'"
-redis_nutcracker_check="netstat -antp|awk  '/7000/ &&  /LISTEN/'"
+redis_sentinel_cehck="netstat -antp|awk  '/26379/ &&  /LISTEN/'"
+redis_nutcracker_check="netstat -antp|awk  '/22121/ &&  /LISTEN/'"
 #redis相关信
 print "==="*30
+
+def log(loglevel,loginfo):
+    time_info=datetime.datetime.now()
+    print time_info,loglevel,loginfo
 
 def ssh_pwd(hostname,username,password,port=22):
     paramiko.util.log_to_file('pwd.log')
@@ -43,16 +46,23 @@ def ssh_pwd(hostname,username,password,port=22):
     ssh.connect(hostname,port,username,password)
     return ssh
 redis_server=ssh_pwd(host["ip"],host["name"],host["password"],host["port"])
-def check_redis_sentinel(redis_server):
-    if redis_server.exec_command(redis_sentinel_cehck)[1].read() is "":
-        print datetime.datetime.now(),"WAR redis_sentinel is not running,start redis_sentinel"
-        redis_server.exec_command(redis_sentinel_start)
-    # else:
-    #     print datetime.datetime.now(),"INFO redis_sentinel_cehck status is OK!"
-    if redis_server.exec_command(redis_nutcracker_check)[1].read() is "":
-        print datetime.datetime.now(),"redis nutcracker is not running,auto start nutcracker"
-        redis_server.exec_command(redis_nutcracker_start)
 
+def check_redis_sentinel(redis_server):
+    redis_sentinel_status=redis_server.exec_command(redis_sentinel_cehck)[1].read()
+    if redis_sentinel_status is "":
+        log("ERROR", "redis_sentinel is not running,start redis_sentinel")
+        redis_server.exec_command(redis_sentinel_start)
+        if redis_sentinel_status is not "":
+           log("INFO", "redis_sentinel Start success!")
+    # else:
+    #    print datetime.datetime.now(),"INFO redis_sentinel_cehck status is OK!"
+def check_nutcracker_status(redis_server):
+    nutcracker_status=redis_server.exec_command(redis_nutcracker_check)[1].read()
+    if nutcracker_status is "":
+        print datetime.datetime.now(),"ERROR redis nutcracker is not running,auto start nutcracker"
+        redis_server.exec_command(redis_nutcracker_start)
+        if nutcracker_status is not "":
+            print datetime.datetime.now(),"redis nutcracker start succsess"
 
 def get_sentinel_info(sentinel_info):
     master_name=re.findall("name=\w+",sentinel_info)
@@ -71,6 +81,7 @@ def get_sentinel_info(sentinel_info):
     return {"redis_master":redis_master,"redis_master_ip":redis_master_ip,"redis_master_name":redis_master_name}
 
 def check_nutcracker(ssh,redis_master):
+    check_nutcracker_status(redis_server)
     redis_sftp=ssh.open_sftp()
     file=redis_sftp.open(nutcracker_file).read()
     #匹配10.237.81.102:7000:1 master
@@ -97,18 +108,19 @@ def check_nutcracker(ssh,redis_master):
     redis_sftp.put(sys.path[0]+"/nutcracker_file",nutcracker_file)
     return "wait_for_restart"
 
-
-#检查Redis、Sentinel、nutcracker
 def redis_master():
     sentinel_info=redis_server.exec_command(redis_sentinel_info)[1].read()
     return get_sentinel_info(sentinel_info)
 
-print datetime.datetime.now(),"INFO Start to monit redis status !"
+log("INFO","Start to monit redis status !")
+#检查Redis、Sentinel、nutcracker
+log("INFO","check redis_sentinel !!!")
+check_redis_sentinel(redis_server)
 print "The redis master ip is",redis_master()["redis_master_ip"]
 while(True):
     time.sleep(check_time)              #程序休息10秒
-    check_redis_sentinel(redis_server)  #检查redis_sentinel 以及TwemProxy 
-
+    check_redis_sentinel(redis_server)
+    #检查redis_sentinel 以及TwemProxy 
     file_info=None
     try:
         file_info=check_nutcracker(redis_server,redis_master())
@@ -121,11 +133,9 @@ while(True):
     if file_info == "success":
         continue
     if file_info =="wait_for_restart":
-        print datetime.datetime.now(),"ERROR restart nutcracker"
+        log("INFO","restart nutcracker")
         redis_server.exec_command(redis_nutcracker_end)
         time.sleep(2)
         redis_server.exec_command(redis_nutcracker_start)
-        print datetime.datetime.now(),"INFO restart nutcracker over!"
+        log("INFO","restart nutcracker over！")
         print "The redis master ip is",redis_master()["redis_master_ip"]
-
-    
