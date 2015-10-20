eb11f843edfd3f1c19807d4bcd74cd8a3bd44c54
benben17 <892100089@qq.com>
2015-10-20 10:40:42 +0800

    redis_sentinel_ check bug 修复

--- redis-twemproxy-monit.py
+++ redis-twemproxy-monit.py
@@ -3,13 +3,15 @@
 __author__ = 'lixiaozhu'
 
 import time
-import re,os
+import re
+import os
 import datetime, paramiko
+
 # nutcracker文件位置
 _nutcracker_file = "/etc/nutcracker/nutcracker.yml"
 # _nutcracker_file = "alpha.xml"
 # 启动sentinel
-_redis_sentinel_start = "nohup redis-sentinel /export/sentinel/sentinel.conf &"
+_redis_sentinel_start = "redis-sentinel /export/sentinel/sentinel.conf"
 # 关闭sentinel
 _redis_sentinel_end = "kill -s 9 `ps -aux | grep redis-sentinel | awk '{print $2}'`"
 # 启动nutcracker
@@ -18,21 +20,24 @@ _redis_nutcracker_start = "nutcracker -d -p /var/log/nutcracker.pid -c " + _nutc
 _redis_nutcracker_end = "cat /var/log/nutcracker.pid |xargs kill -9 && rm -rf /var/log/nutcracker.pid"
 # 查看当前主从信息
 _redis_sentinel_info = "redis-cli -p 26379 info Sentinel|grep status"
-_redis_name_command="redis-cli -p 26379 info Sentinel |awk -F\",\" \'/address/ {print $1}\'|awk -F\"\:name=\" '{printf(\"\%s,\",$2)}'"
+_redis_name_command="redis-cli -p 26379 info Sentinel |awk -F\",\" \'/address/ {print $1}\'|awk -F\":name=\" '{printf(\"%s,\",$2)}'"
 
 # 检查配置时间，单位秒
 check_time = 5
+
 _redis_server = None
 # 检查各个进程的存活情况
+
+
 redis_sentinel_cehck = "netstat -antp|awk  '/26379/ &&  /LISTEN/'"
 redis_nutcracker_check = "netstat -antp|awk  '/22121/ &&  /LISTEN/'"
 
 host=None
-host = {}
-host["ip"] = "10.10.10.1"
-host["name"] = "******"
-host["password"] = "******"
-host["port"] = 22
+# host = {}
+# host["ip"] = "10.xx.xx.xx"
+# host["name"] = "xxxxx"
+# host["password"] = "xxxxxxx"
+# host["port"] = 22
 
 
 def ssh_pkey_pwd(hostname, username, password, privatekey=None, port=22):
@@ -47,6 +52,7 @@ if host is None or {}:
     _redis_server = None
 else:
     _redis_server = ssh_pkey_pwd(host["ip"], host["name"], host["password"], port=host["port"])
+
 
 def run_command(check_command):
     if _redis_server is None:
@@ -65,10 +71,12 @@ _redis_master_name=get_reids_names()
 
 def check_redis_sentinel():
     redis_sentinel_status = run_command(redis_sentinel_cehck)
-    if redis_sentinel_status is "":
+    if redis_sentinel_status == '':
+        print redis_sentinel_status
         log("[ERROR]", "redis_sentinel is not running,start redis_sentinel")
         redis_sentinel_status = run_command(_redis_sentinel_start)
-        if redis_sentinel_status is not "":
+    print redis_sentinel_status
+        if redis_sentinel_status is not '':
             log("[INFO]", "redis_sentinel Start success!")
         else:
             print datetime.datetime.now(), "INFO redis_sentinel_cehck status is OK!"
@@ -77,11 +85,12 @@ def check_redis_sentinel():
 
 
 def check_nutcracker_status():
+    # nutcracker_status=os.popen(redis_nutcracker_check).read()
     nutcracker_status = run_command(redis_nutcracker_check)
-    if nutcracker_status is "":
+    if nutcracker_status == '':
         print datetime.datetime.now(), "ERROR redis nutcracker is not running,auto start nutcracker"
         run_command(_redis_nutcracker_start)
-        if nutcracker_status is not "":
+        if nutcracker_status != 0 :
             print time.ctime(), "redis nutcracker start succsess"
 
 
@@ -96,7 +105,8 @@ class __tw_monit:
 
     def __init__(self, redis_master_name):
         self._redis_master_names = redis_master_name
-        self._redis_master_info = self.redis_master()
+        # self._redis_sentinel_info=self.redis_master()
+        #self.redis_master() = self.redis_master()
 
     def get_sentinel_info(self, sentinel_info):
         if self._redis_master_names is not None:
@@ -132,9 +142,10 @@ class __tw_monit:
         check_nutcracker_status()
         f = self.getfile(_nutcracker_file)
         file = f.read()
-        if self._redis_master_info is not None:
+        if self.redis_master() != '':
             tmp_file = file
-            for redis_master in self._redis_master_info:
+            for redis_master in self.redis_master():
+                # 匹配10.237.81.102:7000:1 master
                 if redis_master is None: continue;
                 config = re.findall("\d+\.\d+\.\d+\.\d+:\d+\:\d+\s*" + redis_master["redis_master_name"] + "\s*\"",file)
                 if config is []:
@@ -142,6 +153,7 @@ class __tw_monit:
                 config_array = re.split(':|\s*', config[0])
                 if len(config_array) < 4:
                     continue
+                # print nutcracker_file+":"+config[0]
                 config_re = {"config_redis": config_array[0] + ":" + config_array[1], "config_right": config_array[2],
                              "config_name": config_array[3]}
                 config_now = config_re['config_redis'] + config_re['config_name']
@@ -150,7 +162,7 @@ class __tw_monit:
                             redis_master["redis_master_name"] + "\"")
             f.close()
             if tmp_file != file:
-                print("exec "+"cp "+_nutcracker_file+"{,."+time.strftime("%Y%m%d-%H%M%S")+"}")
+                print(time.ctime(), "[INFO]"+"cp "+_nutcracker_file+"{,."+time.strftime("%Y%m%d-%H%M%S")+"}")
                 run_command("cp "+_nutcracker_file+"{,."+time.strftime("%Y%m%d-%H%M%S")+"}")
                 # 替换新文件
                 f = self.getfile(_nutcracker_file, "w+")
@@ -160,7 +172,9 @@ class __tw_monit:
             return 0
 
     def redis_master(self):
+        # sentinel_info=os.popen(_redis_sentinel_info).read()
         sentinel_info = run_command(_redis_sentinel_info)
+        # master0:name=master1,status=ok,address=10.160.0.8:6370,slaves=2,sentinels=1
         sentinel_info_list = re.findall(
             "master\d+:name=\w+,status=\w+,address=\d+\.\d+\.\d+\.\d+:\d+,slaves=\d+.sentinels=\d+", sentinel_info)
         redis_list = []
@@ -170,8 +184,8 @@ class __tw_monit:
         return redis_list
 
     def print_master_ip(self):
-        if self._redis_master_info is not None:
-            for _redis_info in self._redis_master_info:
+        if self.redis_master() != '':
+            for _redis_info in self.redis_master():
                 if _redis_info is None: continue
                 print time.ctime(), "[INFO] The redis master ip is---->", _redis_info["redis_master_ip"]
 
@@ -179,13 +193,13 @@ print "---" * 20
 log("[INFO]", "Start to monit redis status !")
 # 检查Redis、Sentinel、nutcracker
 log("[INFO]", "check redis_sentinel !!!")
-check_redis_sentinel()
 
 while (True):
-    _redis = __tw_monit(_redis_master_name)
-    _redis.print_master_ip()
-    time.sleep(check_time)              #程序休息10秒
     check_redis_sentinel()
+    _redis = __tw_monit(get_reids_names())
+    #_redis.print_master_ip()
+    time.sleep(check_time)              #程序休息10秒
+    print "==="*20 
     # 检查redis_sentinel 以及TwemProxy
     file_info = 5
     try:
@@ -197,6 +211,7 @@ while (True):
         print time.ctime(), "[ERROR] Can't get nutcracker information;", _redis.check_nutcracker()
         break
     if (file_info == 0):
+        _redis.print_master_ip()
         continue
     else:
         log("[INFO]", "restart nutcracker")
